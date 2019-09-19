c6f3e8067e7c3af1b0e29764836fdd5df966710b
unknown <malinov.dima18@gmail.com>
2019-09-19 17:34:40 +0300

    Updates

---
+++ precache-manifest.367dfb19dc6ac6032a9efdead4d7e241.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a37596f0322b9fa8b69fe3045905ce",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "44d347f578d5ab765d5e",
+    "url": "/Portfolio/static/css/main.fcac602d.chunk.css"
+  },
+  {
+    "revision": "50511dc6405c08913694",
+    "url": "/Portfolio/static/js/2.e795941d.chunk.js"
+  },
+  {
+    "revision": "44d347f578d5ab765d5e",
+    "url": "/Portfolio/static/js/main.82da4fdd.chunk.js"
+  },
+  {
+    "revision": "477435e083081aef76f5",
+    "url": "/Portfolio/static/js/runtime~main.28447a05.js"
+  },
+  {
+    "revision": "3b70d0fc31c484bc55aaaf84e2262a04",
+    "url": "/Portfolio/static/media/linkedin.3b70d0fc.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e76d8edc24124d0de1a2d318a968de68",
+    "url": "/Portfolio/static/media/redux.e76d8edc.svg"
+  },
+  {
+    "revision": "9ca51d2ac295d3538a2173763f5c7e70",
+    "url": "/Portfolio/static/media/telegram2.9ca51d2a.svg"
+  },
+  {
+    "revision": "8c9399e31ce755970497937f81573e54",
+    "url": "/Portfolio/static/media/vk.8c9399e3.svg"
+  }
+]);
